08c271fa33886f6297cc073b2092aedd0ef6b389
mariomtzrn <mariomtzrn@gmail.com>
2018-09-27 01:29:26 +0000

    Joomla

---
+++ Joomla_3.8.12-Stable-Full_Package/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-09-26 20:44:53 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-09-26T20:44:53+00:00	INFO 187.198.44.211	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ Joomla_3.8.12-Stable-Full_Package/administrator/logs/jcontroller.log.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-09-26 20:59:59 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-09-26T20:59:59+00:00	INFO 187.198.44.211	controller	Holding edit ID com_menus.edit.item.114 Array (     [0] => 101     [1] => 114 ) 
+2018-09-26T21:00:14+00:00	INFO 187.198.44.211	controller	Releasing edit ID com_menus.edit.item.114 Array (     [0] => 101 ) 
+2018-09-26T21:00:32+00:00	INFO 187.198.44.211	controller	Holding edit ID com_menus.edit.item.101 Array (     [0] => 101 ) 
+2018-09-26T21:01:09+00:00	INFO 187.198.44.211	controller	Releasing edit ID com_menus.edit.item.101 Array ( ) 
